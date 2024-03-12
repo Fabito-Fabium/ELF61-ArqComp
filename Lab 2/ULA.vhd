6f651bc9e3d3d6153a997f978b9e8d0eb96b6543
Fabio Zhao Yuan Wang <fabioyuan@gmail.com>
2024-03-12 13:34:16 -0300

    renomeei as variaveis
    basicamente o titulo e deixei mais bonitinho a parte do port map

--- Lab 2/ULA.vhd
+++ Lab 2/ULA.vhd
@@ -4,28 +4,28 @@ use ieee.numeric_std.all;
 
 entity ULA is
 	port( 
-		A: in unsigned (15 downto 0);
-		B: in unsigned (15 downto 0);
-		OP: in unsigned (2 downto 0);
-		S: out unsigned (15 downto 0);
-		zero: out std_logic;
-		LT: out std_logic);
+		in_A: 		in unsigned (15 downto 0);
+		in_B: 		in unsigned (15 downto 0);
+		op: 		in unsigned (2 downto 0);
+		out_ULA: 	out unsigned (15 downto 0);
+		zero: 		out std_logic;
+		LT: 		out std_logic);
 end entity;
 
 architecture ULA_arch of ULA is
  begin
-	s <=    A + B when OP="000" else
-			A - B when OP="001" else
-			A / B when OP="010" else
-			A + B when OP="011" else
-			A and B when OP="100" else
-			A or B when OP="101" else
-			not A when OP="110" else
-			not B when OP="111" else
+	out_ULA <=    	in_A + in_B when op="000" else
+			in_A - in_B when op="001" else
+			in_A / in_B when op="010" else
+			in_A + in_B when op="011" else
+			in_A and in_B when op="100" else
+			in_A or in_B when op="101" else
+			not in_A when op="110" else
+			not in_B when op="111" else
 			"0000000000000000";
 			
-	LT <=	'1' when A < B else
-			'0' when A >= B else
+	LT <=	'1' when in_A < in_B else
+			'0' when in_A >= in_B else
 			'0';
 			
 end architecture;		
--- Lab 2/ULA_tb.vhd
+++ Lab 2/ULA_tb.vhd
@@ -2,46 +2,41 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity ULA_test is
+entity ULA_tb is
 end entity;
 
-architecture ULA_tb of ULA_test is
+architecture Run_tb of ULA_tb is
 component ULA
 	port (
-		A: in unsigned (15 downto 0);
-		B: in unsigned (15 downto 0);
-		OP: in unsigned (2 downto 0);
-		S: out unsigned (15 downto 0);
-		zero: out std_logic;
-		LT: out std_logic);
+		in_A: 		in unsigned (15 downto 0);
+		in_B: 		in unsigned (15 downto 0);
+		op: 		in unsigned (2 downto 0);
+		out_ULA: 	out unsigned (15 downto 0);
+		zero: 		out std_logic;
+		LT:	 	out std_logic);
 end component;
 
-signal A_in: unsigned (15 downto 0);
-signal B_in: unsigned (15 downto 0);
-signal OP_in: unsigned (2 downto 0);
-signal S_out: unsigned (15 downto 0);
-signal zero_out: std_logic;
-signal LT_out: std_logic;
+signal in_A: 	unsigned (15 downto 0);
+signal in_B: 	unsigned (15 downto 0);
+signal op: 	unsigned (2 downto 0);
+signal out_ULA: unsigned (15 downto 0);
+signal zero: 	std_logic;
+signal LT: 	std_logic;
 
 begin
-	uut: ULA port map ( A => A_in,
-						B => B_in,
-						OP => OP_in,
-						S => S_out,
-						zero => zero_out,
-						LT => LT_out);
+	tb: 	ULA port map(in_A, in_B, op, out_ULA, zero, LT);
 	
 	process begin
-		A_in <= "0000000000001000";
-		B_in <= "0000000000000010";
-		OP_in <= "000"; wait for 50 ns;
-		OP_in <= "001"; wait for 50 ns;
-		OP_in <= "010"; wait for 50 ns;
-		OP_in <= "011"; wait for 50 ns;
-		OP_in <= "100"; wait for 50 ns;
-		OP_in <= "101"; wait for 50 ns;
-		OP_in <= "110"; wait for 50 ns;
-		OP_in <= "111"; wait for 50 ns;
+		in_A <= "0000000000001000";
+		in_B <= "0000000000000010";
+		op <= "000"; wait for 50 ns;
+		op <= "001"; wait for 50 ns;
+		op <= "010"; wait for 50 ns;
+		op <= "011"; wait for 50 ns;
+		op <= "100"; wait for 50 ns;
+		op <= "101"; wait for 50 ns;
+		op <= "110"; wait for 50 ns;
+		op <= "111"; wait for 50 ns;
 		wait;
 	end process;
 end architecture;
