0aa939de705466c33ca3addd6d87c218333508fe
Fabio Zhao Yuan Wang <fabioyuan@gmail.com>
2024-03-12 20:50:08 -0300

    Lab2 v1.2
    Principal:
    ** Implementação do 'zero'
    
    miscellaneous:
    ** op:  std_logic_vector
    
    ** Partes que envolvem comparação usei conversão p/ std_logic_vector

--- Lab 2/ULA.vhd
+++ Lab 2/ULA.vhd
@@ -6,26 +6,32 @@ entity ULA is
 	port( 
 		in_A: 		in unsigned (15 downto 0);
 		in_B: 		in unsigned (15 downto 0);
-		op: 		in unsigned (2 downto 0);
+		op: 		in std_logic_vector (2 downto 0);
 		out_ULA: 	out unsigned (15 downto 0);
 		zero: 		out std_logic;
 		LT: 		out std_logic);
 end entity;
 
 architecture ULA_arch of ULA is
- begin
-	out_ULA <=    	in_A + in_B when op="000" else
-			in_A - in_B when op="001" else
-			in_A / in_B when op="010" else
-			in_A + in_B when op="011" else
-			in_A and in_B when op="100" else
-			in_A or in_B when op="101" else
-			not in_A when op="110" else
-			not in_B when op="111" else
+
+signal inter_out: 		unsigned(15 downto 0);
+
+begin
+	inter_out <=   	in_A + in_B 	when op="000" else
+			in_A - in_B 	when op="001" else
+			in_A / in_B 	when op="010" else
+			in_A + in_B 	when op="011" else
+			in_A and in_B 	when op="100" else
+			in_A or in_B 	when op="101" else
+			not in_A 	when op="110" else
+			not in_B 	when op="111" else
 			"0000000000000000";
+	
+	out_ULA <= inter_out;
 			
-	LT <=	'1' when in_A < in_B else
-			'0' when in_A >= in_B else
-			'0';
-			
+	LT <=	'1' when std_logic_vector(in_A) < std_logic_vector(in_B) else
+		'0';
+		
+	zero <= '1' when std_logic_vector(inter_out) = "0000000000000000" else
+		'0';
 end architecture;		
--- Lab 2/ULA_tb.vhd
+++ Lab 2/ULA_tb.vhd
@@ -10,7 +10,7 @@ component ULA
 	port (
 		in_A: 		in unsigned (15 downto 0);
 		in_B: 		in unsigned (15 downto 0);
-		op: 		in unsigned (2 downto 0);
+		op: 		in std_logic_vector (2 downto 0);
 		out_ULA: 	out unsigned (15 downto 0);
 		zero: 		out std_logic;
 		LT:	 	out std_logic);
@@ -18,7 +18,7 @@ end component;
 
 signal in_A: 	unsigned (15 downto 0);
 signal in_B: 	unsigned (15 downto 0);
-signal op: 	unsigned (2 downto 0);
+signal op: 	std_logic_vector (2 downto 0);
 signal out_ULA: unsigned (15 downto 0);
 signal zero: 	std_logic;
 signal LT: 	std_logic;
@@ -27,16 +27,16 @@ begin
 	tb: 	ULA port map(in_A, in_B, op, out_ULA, zero, LT);
 	
 	process begin
-		in_A <= "0000000000001000";
-		in_B <= "0000000000000010";
-		op <= "000"; wait for 50 ns;
-		op <= "001"; wait for 50 ns;
-		op <= "010"; wait for 50 ns;
-		op <= "011"; wait for 50 ns;
-		op <= "100"; wait for 50 ns;
-		op <= "101"; wait for 50 ns;
-		op <= "110"; wait for 50 ns;
-		op <= "111"; wait for 50 ns;
+		in_A <= "0000000000000010";
+		in_B <= "0000000000000100";
+		op <= "000"; wait for 50 ns; --Soma
+		op <= "001"; wait for 50 ns; --subtração
+		op <= "010"; wait for 50 ns; --divisão
+		op <= "011"; wait for 50 ns; --multiplicação****
+		op <= "100"; wait for 50 ns; --AND
+		op <= "101"; wait for 50 ns; --OR
+		op <= "110"; wait for 50 ns; --not(in_A)
+		op <= "111"; wait for 50 ns; --not(in_B)
 		wait;
 	end process;
 end architecture;
