4d715288c88103837a7801ba6b01ea3d5e3974fd
Fabio Zhao Yuan Wang <fabioyuan@gmail.com>
2024-03-29 23:14:39 -0300

    Lab 3 - Master
    *run unit error fixed.
    *RA display implemented

--- Lab 3/Eq01-RegFile.vhd
+++ Lab 3/Eq01-RegFile.vhd
@@ -21,37 +21,30 @@ port( 	clk:		in std_logic;
 end component;
 
 --output from register
-signal o0, o1, o2, o3, o4, o5, o6, o7:	unsigned(15 downto 0);
+signal o0, o1, o2, o3, o4, o5, o6, o7:	unsigned(15 downto 0):=x"0000";
 --write enable register
-signal w0, w1, w2, w3, w4, w5, w6, w7:	std_logic;
+signal wr:				std_logic_vector(7 downto 0);
 
 begin
-rg0:	reg16bits port map(clk, rst, w0, WD3, o0);
-rg1:	reg16bits port map(clk, rst, w1, WD3, o1);
-rg2:	reg16bits port map(clk, rst, w2, WD3, o2);
-rg3:	reg16bits port map(clk, rst, w3, WD3, o3);
-rg4:	reg16bits port map(clk, rst, w4, WD3, o4);
-rg5:	reg16bits port map(clk, rst, w5, WD3, o5);
-rg6:	reg16bits port map(clk, rst, w6, WD3, o6);
-rg7:	reg16bits port map(clk, rst, w7, WD3, o7);
 
-
-w0 <=  	'1' when A3="000" and wr_en='1' else
-	'0';
-w1 <=	'1' when A3="001" and wr_en='1' else
-	'0';
-w2 <=  	'1' when A3="010" and wr_en='1' else
-	'0';
-w3 <=	'1' when A3="011" and wr_en='1' else
-	'0';
-w4 <=  	'1' when A3="100" and wr_en='1' else
-	'0';
-w5 <=	'1' when A3="101" and wr_en='1' else
-	'0';
-w6 <=  	'1' when A3="110" and wr_en='1' else
-	'0';
-w7 <=	'1' when A3="111" and wr_en='1' else
-	'0';
+rg0:	reg16bits port map(clk, rst, wr(0), WD3, o0);
+rg1:	reg16bits port map(clk, rst, wr(1), WD3, o1);
+rg2:	reg16bits port map(clk, rst, wr(2), WD3, o2);
+rg3:	reg16bits port map(clk, rst, wr(3), WD3, o3);
+rg4:	reg16bits port map(clk, rst, wr(4), WD3, o4);
+rg5:	reg16bits port map(clk, rst, wr(5), WD3, o5);
+rg6:	reg16bits port map(clk, rst, wr(6), WD3, o6);
+rg7:	reg16bits port map(clk, rst, wr(7), WD3, o7);
+	
+wr <= 	x"01" when A3="000" and wr_en='1' else
+	x"02" when A3="001" and wr_en='1' else
+	x"04" when A3="010" and wr_en='1' else
+	x"08" when A3="011" and wr_en='1' else
+	x"10" when A3="100" and wr_en='1' else
+	x"20" when A3="101" and wr_en='1' else
+	x"40" when A3="110" and wr_en='1' else
+	x"80" when A3="111" and wr_en='1' else
+	x"00";
 
 RD1 <= 	o0 when A1="000" else
 	o1 when A1="001" else
@@ -72,6 +65,5 @@ RD2 <= 	o0 when A2="000" else
 	o6 when A2="110" else
 	o7 when A2="111" else
 	x"0000";
-
 
 end architecture;
--- Lab 3/Eq01-RegFile_tb.vhd
+++ Lab 3/Eq01-RegFile_tb.vhd
@@ -26,9 +26,9 @@ port( 	clk, rst, wr_en:	in std_logic;
 	);
 end component;
 
-signal clk, rst, wr_en: std_logic;
-signal A1, A2, A3:	unsigned(2 downto 0);
-signal WD3, RD1, RD2:	unsigned(15 downto 0);
+signal clk, rst, wr_en: std_logic		:='0';
+signal A1, A2, A3:	unsigned(2 downto 0)	:= "000";
+signal WD3, RD1, RD2:	unsigned(15 downto 0)	:= x"0000";
 --RA: 2358263 => 8263
 --RA: 2358310 => 8310
 --RA: 2378345 => 8345
@@ -37,53 +37,42 @@ begin
 	tb: 	RegFile port map(clk, rst, wr_en, A1, A2, A3, WD3, RD1, RD2);
 	
 	process begin
-		--config inicial p/ deixar o ghw mais legivel
-		rst	<= '0';
-		clk 	<= '0';
-		wr_en 	<= '0';
-		WD3	<= x"0000";
-		A3	<= "000";	wait for 50 ns; --para explicitar reset
-		
 		--reset explicito
-		rst 	<= '1'; 	wait for 50 ns;
+		rst 	<= '0'; 	wait for 50 ns;
+		rst	<= '1';		wait for 50 ns;
 		rst	<= '0';		wait for 50 ns;
 		
-		
-		WD3	<= x"1111"; 	wait for 50 ns;
-		wr_en 	<= '1';		wait for 50 ns;
-		A3	<= "000";	wait for 50 ns;
-		clk 	<= '1'; 	wait for 50 ns;
-		clk 	<= '0';		wait for 50 ns;
-		wr_en	<= '0';		wait for 50 ns;
-		
-		
-		WD3	<= x"4567"; 	wait for 50 ns;
-		wr_en 	<= '1';		wait for 50 ns;
+		WD3	<= to_unsigned(10#008263#, WD3'length); 	
 		A3	<= "001";	wait for 50 ns;
+		wr_en	<= '1';		wait for 50 ns;
 		clk 	<= '1'; 	wait for 50 ns;
 		clk 	<= '0';		wait for 50 ns;
 		wr_en	<= '0';		wait for 50 ns;
 		
-		
-		WD3	<= x"89AB"; 	wait for 50 ns;
-		wr_en 	<= '1';		wait for 50 ns;
+		WD3	<= to_unsigned(10#008310#, WD3'length); 	
 		A3	<= "010";	wait for 50 ns;
+		wr_en	<= '1';		wait for 50 ns;
 		clk 	<= '1'; 	wait for 50 ns;
 		clk 	<= '0';		wait for 50 ns;
 		wr_en	<= '0';		wait for 50 ns;
 		
-		
-		WD3	<= x"CDEF"; 	wait for 50 ns;
-		wr_en 	<= '1';		wait for 50 ns;
+		WD3	<= to_unsigned(10#008345#, WD3'length); 	
 		A3	<= "011";	wait for 50 ns;
+		wr_en	<= '1';		wait for 50 ns;
 		clk 	<= '1'; 	wait for 50 ns;
 		clk 	<= '0';		wait for 50 ns;
 		wr_en	<= '0';		wait for 50 ns;
 		
-		A1	<= "000";	wait for 50 ns;
-		A2	<= "001";	wait for 50 ns;
-		A1	<= "010";	wait for 50 ns;
-		A2	<= "011";	wait for 50 ns;
+		A2	<= "001"; 	wait for 50 ns;
+		A2	<= "010"; 	wait for 50 ns;
+		A2	<= "011"; 	wait for 50 ns;
+		A2	<= "111";	
+		
+		A1	<= "001"; 	wait for 50 ns;
+		A1	<= "010"; 	wait for 50 ns;
+		A1	<= "011"; 	wait for 50 ns;
+		A1	<= "110";
+		
 		wait;
 		
 	end process;
